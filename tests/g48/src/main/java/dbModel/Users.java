b98c256064fd4f97eba58d2ad2978158c3563764
alina <alinashulga3@gmail.com>
2021-04-28 13:19:39 +0300

    Homework6 add pom

---
+++ tests/g48/src/main/java/dbModel/Users.java
@@ -0,0 +1,12 @@
+package dbModel;
+
+
+import org.javalite.activejdbc.Model;
+import org.javalite.activejdbc.annotations.DbName;
+import org.javalite.activejdbc.annotations.Table;
+
+@DbName("study")
+@Table("data.users")
+public class Users extends Model {
+}
+
--- tests/g48/src/test/java/test/LoginPageTest.java
+++ tests/g48/src/test/java/test/LoginPageTest.java
@@ -1,40 +1,51 @@
 package test;
 
-import Model.Pages.issues.IssuesPage;
+import dbModel.Users;
 import org.junit.Before;
 import org.junit.Test;
+import Model.Pages.issues.IssuesPage;
+import jdk.jfr.Description;
 import Model.LoginPages.LoginPage;
 
 public class LoginPageTest extends BaseTest{
+    @Feature("Positive authorization")
+    @Story("Github auth")
+    @Owner("AlinaSHulha")
+    @Severity(SeverityLevel.NORMAL)
+    public class LoginPageTest extends BaseTest{
 
 
-    private LoginPage loginPage;
+        private LoginPage loginPage;
 
-    @Before
-    public void setUp(){
-        loginPage = new LoginPage(driver);
-    }
+        @Before
+        public void setUp(){
+            loginPage = new LoginPage(driver);
+        }
 
-    @Test
-    public void checkNegativeLogin(){
-        loginPage.login("AlinaSHulha","Richard123789456");
-        loginPage.validateErrorMessage("Incorrect username or password.");
-    }
+        @Test
+        public void checkNegativeLogin(){
+            Users user = Users.findById(1);
+            loginPage.login(
+                    user.getString("username"),
+                    user.getString("password"));
+            loginPage.validateErrorMessage("Incorrect username or password.");
+        }
 
-    @Test
-    public void showCommitMessageTest(){
-        loginPage.login(System.getProperty("AlinaSHulha"), System.getProperty("Richard123789456"))
-                .openProjectG48()
-                .showCommitMessage()
-                .logout();
-    }
-}
-//    @Test
-//    public void readTab(){
-//        loginPage.login("AlinaSHulha","Richard123789456")
-//                .openProjectG48();
-//        LoginPage.homeTabs();
-//    }
+
+        @Description("Тест нужен чтобы показать последнее сообщение коммита")
+        @TmsLink("MR-25")
+        @Issues({
+                @Issue("MR-26"),
+                @Issue("")}
+        )
+        @DisplayName("Показать сообщение коммита")
+        @Test
+        public void showCommitMessageTest(){
+            loginPage.login(System.getProperty("username"), System.getProperty("password"))
+                    .openProjectG48()
+                    .showCommitMessage()
+                    .logout();
+        }
 
 
 
